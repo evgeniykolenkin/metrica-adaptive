fc73f20806e3d7ca11cc6f14c83886ba299d05b4
Evgeny Kolenkin <immaim@yandex.ru>
2023-06-29 13:37:14 +0300

    calculator html, css, js

---
+++ script/calculator.js
@@ -0,0 +1,84 @@
+// константы из HTML
+const selectPlace = document.querySelector(".calculator__place");
+const resultCost = document.querySelector(".calculator__result-cost");
+const inputRange = document.querySelector(".input__range");
+const inputRangeNumber = document.querySelector(".calculator__range-number");
+const resultTotal = document.querySelector(".calculator__result-total");
+const compaundData = document.querySelector(".calculator__compaund-data");
+const additionsList = document.querySelector(".calculator__additions-list");
+const extrasList = document.querySelector(".calculator__extras-list");
+
+// изменяемые переменные
+let TOTAL_COST = 44000;
+let SQUARE_METER_COST = 1000;
+
+// функции-------------------------------------------
+function totalCost() {
+  let cost = inputRange.value * SQUARE_METER_COST;
+  inputRangeNumber.innerHTML = inputRange.value;
+  resultTotal.innerText = `${cost} ₽`;
+
+  let realCheckbox = document.querySelectorAll(".real__checkbox");
+
+  for (input of realCheckbox) {
+    if (input.checked) {
+      cost += Number(input.value);
+      resultTotal.innerText = `${cost} ₽`;
+    }
+  }
+
+  return cost;
+}
+
+function countSquareCost(e) {
+  if (e.target.value === "квартира") {
+    SQUARE_METER_COST = 1000;
+    resultCost.innerText = `${SQUARE_METER_COST} ₽/м²`;
+    totalCost();
+  }
+  if (e.target.value === "дом") {
+    SQUARE_METER_COST = 1500;
+    resultCost.innerText = `${SQUARE_METER_COST} ₽/м²`;
+    totalCost();
+  }
+}
+
+function subtraction(e) {
+  const parentNode = e.target.closest(".calculator__compaund-item");
+  const span = parentNode.querySelector(".compaund__amount");
+  let amount = Number(span.innerHTML);
+  if (e.target.dataset.action === "minus") {
+    const newAmount = amount - 1;
+    span.innerHTML = newAmount;
+  }
+  if (amount <= 0) {
+    span.innerHTML = 0;
+  }
+}
+
+function addition(e) {
+  const parentNode = e.target.closest(".calculator__compaund-item");
+  const span = parentNode.querySelector(".compaund__amount");
+  let amount = Number(span.innerHTML);
+  if (e.target.dataset.action === "plus") {
+    const newAmount = amount + 1;
+    span.innerHTML = newAmount;
+  }
+}
+
+function colorLabel(e) {
+  const parentNode = e.target.closest(".calculator__additions-item");
+  if (e.target.dataset.action === "check") {
+    const span = parentNode.querySelector(".hidden__checkbox");
+    parentNode.classList.toggle("addition__checked");
+    span.classList.toggle("show__checkbox");
+  }
+}
+
+// слушатели событий
+inputRange.addEventListener("input", totalCost);
+selectPlace.addEventListener("change", countSquareCost);
+compaundData.addEventListener("click", subtraction);
+compaundData.addEventListener("click", addition);
+additionsList.addEventListener("click", colorLabel);
+extrasList.addEventListener("click", totalCost);
